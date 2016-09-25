6ed959ba7daafea94b3706bf1ebe519ad8510d31
Sulaiman.Allen <xalerons@gmail.com>
2016-09-24 23:10:56 -0500

    Initial content added

---
+++ git-cheater.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# A script for updating git once a day if no commit has been added that day.
+# Created in reaction to a company's idea that hiring practice dictates choosing
+# a candidate should be based upon the number of commits that person has on github.
+
