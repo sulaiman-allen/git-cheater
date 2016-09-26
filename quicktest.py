584cfb54138fd54cf615862b04b5e42b21e29b30
Sulaiman.Allen <xalerons@gmail.com>
2016-09-26 18:14:06 -0500

    Added logic in python script for checking if a commit was made for the current date

--- quicktest.py
+++ quicktest.py
@@ -1,21 +1,26 @@
 import requests
+from datetime import *
 from bs4 import BeautifulSoup
 
 def main():
 
-    url = "https://github.com/sulaiman-allen?tab=overview&from=2016-03-10"
-    html = requests.get(url)
-    html = html
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    git_username = "sulaiman-allen"
+    base_url = "https://github.com/" + git_username + "?tab=overview&from=" + current_date
+    #base_url = "https://github.com/sulaiman-allen?tab=overview&from=2016-03-01"
+
+    html = requests.get(base_url)
     soup = BeautifulSoup(html.content, "lxml")
     results = soup.find_all('span', class_='text-gray m-0')
 
-    for result in results:
-        print("result = ", result.string)
-        print("type of result = ", type(result.string))
-        if "had no activity during this period" in str(result.string.encode("utf-8")):
-            print("true")
-        else:
-            print("false")
+    # if a commit wasn't made for today...
+    if results:
+        for result in results:
+            #print("result = ", result.string)
+            if "had no activity during this period" in str(result.string.encode("utf-8")):
+                print("true")
+    else:
+        print("commits were made for this day")
 
 
 
