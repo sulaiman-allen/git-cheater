98fa207982ae2623eb4a993af22c2f2900e82069
sulaiman-allen <xalerons@gmail.com>
2016-09-27 10:25:43 -0500

    fixed checking logic to account for present tense

--- git-cheater.py
+++ git-cheater.py
@@ -8,12 +8,12 @@ from bs4 import BeautifulSoup
 def main():
 
     with open('enter_git_username_here.txt') as inputfile:
-        git_username = inputfile.readlines()[0]
+        git_username = inputfile.readlines()[0].rstrip()
 
     current_date = datetime.now().strftime("%Y-%m-%d")
     base_url = "https://github.com/" + git_username + "?tab=overview&from=" + current_date
     #base_url = "https://github.com/sulaiman-allen?tab=overview&from=2016-03-01"
-
+    
     commit_rolodex = ['Just checking to see if anyone is actually reading these things',
                       'Updated method to reference absolute pointer position',
                       'Made a reference to a subprocess call',
@@ -35,16 +35,18 @@ def main():
     # if a commit wasn't made for today...
     if results:
         for result in results:
-            if "had no activity during this period" in str(result.string.encode("utf-8")):
+            if "has no activity yet for this period" in str(result.string.encode("utf-8")):
                 print("no commits made for today")
 
-                with open('scumbag.txt', "a") as outfile:
+                with open('scumbag.txt', "a+") as outfile:
                     outfile.write("Another unsuccesful day\n")
 
                 subprocess.call(['git', 'add', 'scumbag.txt'])
                 subprocess.call(['git', 'commit', '-m', chosen_commit])
                 subprocess.call(['git', 'push', 'origin', 'master'])
 
+            elif "had no activity during this period" in str(result.string.encode("utf-8")):
+                print("disregard this, this is just for checking past events.")
     else:
         print("commits were made for this day")
         subprocess.call(['rm', 'scumbag.txt'])
