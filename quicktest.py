c20f5a0c54853debb3cf04bb0bb5ef9581b9e458
Sulaiman.Allen <xalerons@gmail.com>
2016-09-26 19:07:51 -0500

    Loading of username from file added

--- quicktest.py
+++ quicktest.py
@@ -1,11 +1,14 @@
 import requests
+import subprocess
 from datetime import *
 from bs4 import BeautifulSoup
 
 def main():
 
+    with open('enter_git_username_here.txt') as inputfile:
+        git_username = inputfile.readlines()[0]
+        print("git username =", git_username)
     current_date = datetime.now().strftime("%Y-%m-%d")
-    git_username = "sulaiman-allen"
     base_url = "https://github.com/" + git_username + "?tab=overview&from=" + current_date
     #base_url = "https://github.com/sulaiman-allen?tab=overview&from=2016-03-01"
 
@@ -16,11 +19,17 @@ def main():
     # if a commit wasn't made for today...
     if results:
         for result in results:
-            #print("result = ", result.string)
             if "had no activity during this period" in str(result.string.encode("utf-8")):
-                print("true")
+                print("no commits made for today")
+                with open('scumbag.txt', "a") as outfile:
+                    outfile.write("Another unsuccesful day\n")
+                subprocess.call(['git', 'add', 'scumbag.txt'])
+                subprocess.call(['git', 'commit', '-m', 'testing if this stupid thing works'])
+                subprocess.call(['git', 'push', 'origin', 'master'])
+
     else:
         print("commits were made for this day")
+        subprocess.call(['rm', 'scumbag.txt'])
 
 
 
