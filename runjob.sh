4311de9b667996addf7a82a933eb28358351b125
Sulaiman.Allen <xalerons@gmail.com>
2016-09-26 21:42:01 -0500

    Updated some comments on runjob.sh

--- runjob.sh
+++ runjob.sh
@@ -4,5 +4,9 @@
 # Created in reaction to a company's idea that hiring practice dictates choosing
 # a candidate should be based upon the number of commits that person has on github.
 
+# Cron runs tasks from the users home directory. To reference path locations in the future
+# of this script and the python one, the directory needs to be changed.
 cd git-cheater
+
+# Main functionality happens here
 python ./git-cheater.py
